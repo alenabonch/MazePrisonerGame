24ee887a70d441198dcadd2a56a52a5f4dd1fae9
unknown <tiggy@mail.ru>
2015-06-20 21:53:22 +0400

    Initial commit

---
+++ desktop/src/com/epam/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.epam.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.epam.game.MazePrisonerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MazePrisonerGame(), config);
+	}
+}
---
+++ html/src/com/epam/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.epam.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.epam.game.MazePrisonerGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MazePrisonerGame();
+        }
+}
