45435da24fda56426dfc5d2ad2a1a5a11bf899d6
Alenka <tiggy@mail.ru>
2015-07-02 21:56:05 +0400

    test commit

--- core/src/com/epam/game/game/view/Graphics.java
+++ core/src/com/epam/game/game/view/Graphics.java
@@ -2,7 +2,6 @@ package com.epam.game.game.view;
 
 public interface Graphics {
 
-	void fillRect(int x, int y, int width, int heigth,
-				  int textureIndex);
+	void fillRect(int x, int y, int width, int heigth, int textureIndex);
 
 }
